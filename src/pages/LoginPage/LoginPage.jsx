2d66a0b0ea74d11e27b6e815c20e279aa1e907ee
Cho SangHyun <98shcho@naver.com>
2022-09-22 00:55:35 +0900

    design: add LoginPage design

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,8 +3,33 @@ import styles from './LoginPage.module.css';
 
 const LoginPage = (props) => {
     return(
-        <div>
-            LoginPage
+        <div className={styles.login_page}>
+            <form className={styles.login_form}>
+                <h1 className={styles.login_text}>로그인</h1>
+                <div className={styles.login_input_box}>
+                    <label className={styles.label} htmlFor={styles.email}>이메일</label>
+                    <input placeholder='ex.teentam@gmail.com' 
+                        className={styles.login_input} 
+                        id={styles.email} 
+                        type="email" 
+                        name="email"
+                    />
+                    <p className={`${styles.error_message} ${styles.email_error}`}></p>
+                </div>
+                <div className={styles.login_input_box}>
+                    <label className={styles.label} htmlFor={styles.password}>비밀번호</label>
+                    <input 
+                        placeholder='비밀번호'
+                        className={styles.login_input} 
+                        id={styles.password} 
+                        type="password" 
+                        name="password"
+                    />
+                    <p className={`${styles.error_message} ${styles.password_error}`}></p>
+                </div>
+                <button className={styles.login_button}>로그인</button>
+                <p className={styles.go_to_signup}>처음이신가요? <span className={styles.link_signup}>회원가입</span></p>
+            </form>
         </div>
     )
 }
