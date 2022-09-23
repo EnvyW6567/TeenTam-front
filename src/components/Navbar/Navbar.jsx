e3edb91843c471974aa5a42a50e94f45a3f02fe1
Cho SangHyun <98shcho@naver.com>
2022-09-23 12:17:38 +0900

    comment: add comment to Navbar & Footer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './Navbar.module.css';
 import logo from '../../assets/images/logo.png';
 
+// Nabigation Bar역할의 컴포넌트
+// 로그인 전 후로 다른 모습 보여주도록 추후 작업이 필요
+// 각 메뉴들을 클릭했을 때 해당 페이지로 연결하도록 하는 작업도 필요
+
 const Navbar = (props) => {
     return(
         <nav className={styles.navbar}>
