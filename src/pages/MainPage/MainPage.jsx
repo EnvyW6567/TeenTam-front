2693ae19f4d5f0cab708b1c05095239583169034
Cho SangHyun <98shcho@naver.com>
2022-09-27 01:24:27 +0900

    feat: add user state to MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import MainPageMainSection from '../../components/MainPageMainSection/MainPageMainSection';
 import MainPageSideSection from '../../components/MainPageSideSection/MainPageSideSection';
 import Navbar from '../../components/Navbar/Navbar';
 import styles from './MainPage.module.css';
 
-const MainPage = (props) => {
+const MainPage = ({authService}) => {
+    const location = useLocation();
+
+    const [user, setUser] = useState(location.state ? location.state : {});
+
+    const logout = () => {
+        authService.logout(() => {setUser({})});
+    }
     return(
         <div className={styles.main_page}>
-            <Navbar />
+            <Navbar user={user} logout={logout} />
             <section className={styles.main}>
                 <MainPageMainSection />
-                <MainPageSideSection />
+                <MainPageSideSection user={user}/>
             </section>
             <Footer />
         </div>
