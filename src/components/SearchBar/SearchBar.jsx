59a6abfb649e54afd9c741777e4153ea98ea542c
Cho SangHyun <98shcho@naver.com>
2022-11-10 23:03:33 +0900

    feat(검색기능): add feature to search to SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { IoSearch } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
 import styles from './SearchBar.module.css';
 
 // 검색창 컴포넌트
@@ -7,14 +8,36 @@ import styles from './SearchBar.module.css';
 // navbar안에 위치할 경우 사이즈 조정에 대한 작업도 필요
 
 const SearchBar = ({isInMainPage}) => {
+    const keywordRef = useRef();
+    const navigate = useNavigate();
+
+    const handleKeyDownEnter = (e) => {
+        if (e.key !== "Enter") {
+            return;
+        }
+
+        let keyword = keywordRef.current.value;
+        
+        if (!keyword) {
+            alert("검색할 단어를 입력해주세요");
+            return;
+        }
+
+        keyword = keyword.replaceAll(" ", "+");
+        navigate(`/boards/search?keyword=${keyword}`);
+    }
+
     return(
         <div className={`${styles.search_bar} ${isInMainPage?styles.in_main:styles.not_in_main}`}>
             <IoSearch className={styles.search_icon}/>
             <input 
+                onKeyDown={handleKeyDownEnter}
                 className={styles.search} 
                 placeholder='궁금한 토픽을 검색해보세요'
                 type="text" 
-                name='query'/>
+                name='query'
+                ref={keywordRef}
+            />
         </div>
     )
 }
