3f64b91bbec45ca6b1466f0c500eeedceff824f9
Cho SangHyun <98shcho@naver.com>
2022-11-16 20:06:55 +0900

    feat(급식조회기능): add feature to get luncj info by school name

--- src/services/school_lunch.js
+++ src/services/school_lunch.js
@@ -1,10 +1,22 @@
+// 학교정보 가져오기
+export const getSchoolInfo = async () => {
+    const schoolName = "죽전고등학교";
+    const url = `https://open.neis.go.kr/hub/schoolInfo?KEY=${process.env.REACT_APP_SCHOOL_API_KEY}&Type=json&pIndex=1&pSize=100&SCHUL_NM=${schoolName}`;
+    const res = await fetch(url);
+    const resJson = await res.json();
+    const { ATPT_OFCDC_SC_CODE, SD_SCHUL_CODE } = resJson.schoolInfo[1].row[0];
+
+    return { ATPT_OFCDC_SC_CODE, SD_SCHUL_CODE };
+}
+
 // 점심, 저녁 정보 가져오기. 나중에 하드코딩된 부분 고쳐야 함(교육청코드, 학교코드 받아서 하는 걸로)
 export const getSchoolLunch = async () => {
     const data = {};
     const todayString = getTodayString();
+    const { ATPT_OFCDC_SC_CODE, SD_SCHUL_CODE } = await getSchoolInfo();
     
-    const lunchUrl = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_SCHOOL_API_KEY}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531379&MMEAL_SC_CODE=2&MLSV_YMD=${todayString}`;
-    const dinnerUrl = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_SCHOOL_API_KEY}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=J10&SD_SCHUL_CODE=7531379&MMEAL_SC_CODE=3&MLSV_YMD=${todayString}`;
+    const lunchUrl = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_SCHOOL_API_KEY}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&MMEAL_SC_CODE=2&MLSV_YMD=${todayString}`;
+    const dinnerUrl = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.REACT_APP_SCHOOL_API_KEY}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&MMEAL_SC_CODE=3&MLSV_YMD=${todayString}`;
     
     const lunchRes = await fetch(lunchUrl);
     const dinnerRes = await fetch(dinnerUrl);
